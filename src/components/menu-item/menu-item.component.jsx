b84fc126f62e4aa8dfa637e5344be8d027134c4c
gaston levy mayo <glevymayo@gmail.com>
2020-09-07 18:03:11 -0400

    Agregada pagina shop con el collectionItem

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => (
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
     <div className={`${size} menu-item`}>
         <div className="background-image"
              style={{
                  backgroundImage:`url(${imageUrl})`
              }} />
-        <div className="content">
+        <div className="content" onClick={ () => history.push(`${linkUrl}`)}>
             <div className="title">{title.toUpperCase()}</div>
             <div className="subtitle">SHOP NOW</div>
         </div>
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
